3211288824ce1a8578f791149b92465a3dd7c283
Thomas GERVAIS <thomas.gervais@st.com>
2015-08-10 17:59:13 +0200

    [Upd] FilePlugin Support

--- Interfaces/IFilePlugin.cs
+++ Interfaces/IFilePlugin.cs
@@ -1,11 +1,11 @@
 using System.IO;
 using System.ComponentModel.Composition;
-using PA.Plugin.Operations;
+using PA.Plugin.Operations.Interfaces;
 
 namespace PA.Plugin.File.Interfaces
 {
     [InheritedExport]
-    public interface IFilePlugin : IPlugin, IJobPlugin
+    public interface IFilePlugin : IPlugin, IActionPlugin
     {
         FileInfo File { get; }
     }
